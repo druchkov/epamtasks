0859781a8ff1dae863b3e955568cb5fb833a5a66
New Sheriff in Town <d.druchkov@gmail.com>
2019-09-30 18:32:19 +0300

    added directory for file from task io, and rename Class on correct name

--- src/main/java/io/MainIO.java
+++ src/main/java/io/MainIO.java
@@ -3,11 +3,11 @@ package io;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class ZXS {
+public class MainIO {
     public static void main(String[] args) {
         TreeF tree = new TreeF();
         try {
-            tree.treeF("F:\\downloads\\панкрок", "IO/uot.txt");
+            tree.treeF("F:\\downloads\\панкрок", "IO/out.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
