0e000554a7ad1686edd79d5b31f419e1a140c85e
New Sheriff in Town <d.druchkov@gmail.com>
2019-10-26 19:23:28 +0300

    add page object for yandex disk

---
+++ src/main/java/webdriver/screen/yandex/disk/YandexDiskLoginPageObject.java
@@ -0,0 +1,42 @@
+package webdriver.screen.yandex.disk;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import webdriver.browser.Browser;
+
+public class YandexDiskLoginPageObject {
+    private static final String URL = "https://passport.yandex.ru/auth?from=cloud&origin=disk_landing2_signin_" +
+            "ru&retpath=https%3A%2F%2Fdisk.yandex.ru%3Fsource%3Dlanding2_signin_ru&" +
+            "backpath=https%3A%2F%2Fdisk.yandex.ru%3F";
+    private static final By FIELD_FOR_LOGIN_LOCATOR = By.id("passp-field-login");
+    private static final By BUTTON_LOGIN_LOCATOR = By.xpath("//*[@* = 'submit']");
+    private static final By FIELD_FOR_PASSWORD_LOCATOR = By.id("passp-field-passwd");
+    Browser browser;
+
+    public YandexDiskLoginPageObject(WebDriver driver) {
+        if (browser == null) {
+            browser = new Browser(driver);
+        }
+    }
+
+    public YandexDiskLoginPageObject openPage() {
+        browser.getDriver().manage().window().maximize();
+        browser.getDriver().get(URL);
+        return this;
+    }
+
+    public YandexDiskLoginPageObject typeLogin(String login) {
+        browser.typeText(FIELD_FOR_LOGIN_LOCATOR, login);
+        return this;
+    }
+
+    public YandexDiskLoginPageObject clickOnLogin() {
+        browser.clickOnButton(BUTTON_LOGIN_LOCATOR);
+        return this;
+    }
+
+    public YandexDiskLoginPageObject typePassword(String password) {
+        browser.typeText(FIELD_FOR_PASSWORD_LOCATOR, password);
+        return this;
+    }
+}
---
+++ src/main/java/webdriver/screen/yandex/disk/YandexDiskPageObject.java
@@ -0,0 +1,59 @@
+package webdriver.screen.yandex.disk;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import webdriver.browser.Browser;
+
+public class YandexDiskPageObject {
+    private static final By BUTTON_CREATE_LOCATOR = By.xpath("//div[@class = 'sidebar__buttons']//button");
+    private static final By BUTTON_CREATE_NEW_FOLDER_LOCATOR = By.xpath("//span[text() = 'Папку']/..");
+    private static final By FIELD_FOR_CREATE_NEW_NAME_FILE_LOCATOR =
+            By.xpath("//form[@class = 'rename-dialog__rename-form']//input");
+    private static final By BUTTON_SAVE_LOCATOR = By.xpath("//div[@class = 'dialog__body']//button");
+    private static final By TRASH_LOCATOR = By.xpath("//div[contains(@class, 'js-prevent-drag')]");
+    private static final By FIELD_ITEMS_LOCATOR = By.xpath("//div[@class = 'client-listing']");
+    private static final By NEW_FOLDER_FROM_CONTEXT_MENU_LOCATOR =
+            By.xpath("//div[@class = 'menu__group']/div[contains(@class, '_new-folder')]");
+    Browser browser;
+
+    public YandexDiskPageObject(WebDriver driver) {
+        if (browser == null) {
+            browser = new Browser(driver);
+        }
+    }
+
+    private By createLocatorForFilesByName(String name) {
+        String locator = String.
+                format("//span[text() = '%s']/ancestor::div[contains(@class, 'listing-item_size_m')]", name);
+        return By.xpath(locator);
+    }
+
+    public YandexDiskPageObject createNewFolder(String nameFolder) {
+        browser.clickOnButton(BUTTON_CREATE_LOCATOR);
+        browser.clickOnButton(BUTTON_CREATE_NEW_FOLDER_LOCATOR);
+        browser.typeTextWithPreliminaryClearField(FIELD_FOR_CREATE_NEW_NAME_FILE_LOCATOR, nameFolder);
+        browser.clickOnButton(BUTTON_SAVE_LOCATOR);
+        return this;
+    }
+
+    public YandexDiskPageObject moveFileInTresh(String nameFile) {
+        browser.moveItemInOtherItem(createLocatorForFilesByName(nameFile), TRASH_LOCATOR);
+        return this;
+    }
+
+    public YandexDiskPageObject createFolderFromContextMenu(String nameFolder) {
+        browser.contextClick(FIELD_ITEMS_LOCATOR);
+        browser.clickOnButton(NEW_FOLDER_FROM_CONTEXT_MENU_LOCATOR);
+        browser.typeTextWithPreliminaryClearField(FIELD_FOR_CREATE_NEW_NAME_FILE_LOCATOR, nameFolder);
+        browser.clickOnButton(BUTTON_SAVE_LOCATOR);
+        return this;
+    }
+
+    public boolean isFileExist(String name) {
+        return new WebDriverWait(browser.getDriver(),1)
+                .until(ExpectedConditions.visibilityOfElementLocated(createLocatorForFilesByName(name)))
+                .isDisplayed();
+    }
+}
