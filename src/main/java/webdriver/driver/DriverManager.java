4d8600ac278e4d9ab00bb96eabc5995064d01546
New Sheriff in Town <d.druchkov@gmail.com>
2019-11-08 17:45:58 +0300

    added model user yandex disk and webdriver manager

---
+++ src/main/java/webdriver/driver/DriverManager.java
@@ -0,0 +1,39 @@
+package webdriver.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class DriverManager {
+    private static WebDriver driver;
+
+    private DriverManager() {
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            switch (System.getProperty("browser")) {
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "opera":
+                    WebDriverManager.operadriver().setup();
+                    driver = new OperaDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void quiteDriver() {
+        driver.quit();
+        driver = null;
+    }
+}
