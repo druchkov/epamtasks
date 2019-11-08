e19e050300771806e067d104fa19b17f9411e8ec
New Sheriff in Town <d.druchkov@gmail.com>
2019-11-08 17:36:19 +0300

    added TestListener

---
+++ src/main/java/webdriver/util/TestListener.java
@@ -0,0 +1,73 @@
+package webdriver.util;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import webdriver.driver.DriverManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TestListener implements ITestListener {
+    private Logger log = LogManager.getRootLogger();
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        screenshot();
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+
+    private void screenshot() {
+        File screen = ((TakesScreenshot) DriverManager.getDriver())
+                .getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screen, new File(
+                    ".//target/screenshots/" + getCurrentTime()
+                            + ".png"
+            ));
+        } catch (IOException exc) {
+            log.error(exc.getLocalizedMessage());
+        }
+    }
+
+    private String getCurrentTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd_HH-mm-ss");
+        return ZonedDateTime.now().format(formatter);
+    }
+}
