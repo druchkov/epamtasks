cfbcf5d85c2b4ff1b23b71fbb9080ac7c73d6b80
Denis Druchkov <d.druchkov@gmail.com>
2019-12-09 22:55:53 +0300

    add dataTable and implement with are parameters

--- src/main/java/webdriver/steps/LoginPageSteps.java
+++ src/main/java/webdriver/steps/LoginPageSteps.java
@@ -1,5 +1,6 @@
 package webdriver.steps;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -8,14 +9,16 @@ import org.testng.Assert;
 import webdriver.factory.UserYandexDiskFactory;
 import webdriver.screen.yandex.disk.LoginPage;
 
+import java.util.Map;
+
 public class LoginPageSteps {
 
-    @Given("^(The )?[Uu]ser opens? yandex disk page$")
+    @Given("^(The )?[Uu]ser (?:opens?|navigetes? to) (?:[the ]?[Hh]ome page|[the ]?[Yy]andex disk page)$")
     public void userOpenedYandexDiskPage() {
         new LoginPage().open();
     }
 
-    @When("^(The )?[Uu]ser login with correct credentials$")
+    @When("^(The )?[Uu]ser (?:logins?|enters?) with correct credentials$")
     public void loginWithCorrectCredentials() {
         new LoginPage().login(UserYandexDiskFactory.withCredentialsFromProperty());
     }
@@ -26,13 +29,21 @@ public class LoginPageSteps {
     }
 
     @Then("^(The )?[Uu]ser sees error message with text ([\\w]+)$")
-    public void error(String text) {
-        String errorMessage = new LoginPage().getTextMessage();
-        Assert.assertEquals(text, errorMessage, "Messages don't equals");
+    public void userSeesErrorMessage(String text) {
+        String errorMessage = new LoginPage().getErrorTextMessage();
+        Assert.assertEquals(text, errorMessage, "Message does't equals");
     }
 
-    @When("^(The )?[Uu]ser types ([\\w]+) in filed for login name$")
+    @When("^(The )?[Uu]ser types ([\\w]+) (in filed for)? login name$")
     public void typeLogin(String login) {
         new LoginPage().typeLogin(login);
+    }
+
+    @When("^(The )?[Uu]ser logins? with parameters?:$")
+    public void userLoginWithParameters(DataTable userWithParameters) {
+        Map<String, String> user = userWithParameters.asMap(String.class, String.class);
+        new LoginPage()
+                .login(UserYandexDiskFactory
+                        .withAnyCredentials(user.get("loginName"), user.get("password")));
     }
 }
--- src/test/resources/features/LoginWithCorrectCredentials.feature
+++ src/test/resources/features/LoginWithCorrectCredentials.feature
@@ -2,5 +2,7 @@ Feature: Login in Yandex disk with correct credentials
 
   Scenario: Login yandex disk with correct login name
     Given User opens yandex disk page
-    When User login with correct credentials
+    When User logins with parameters:
+      | loginName         | password |
+      | qaAutomationGomel | gomelQa  |
     Then User riches yandex disk
